f9ff0c6c8610d851b19088271f45f98b04af760a
PaulRusz <paulruszkay@gmail.com>
2024-04-07 13:26:11 -0400

    added code to index.js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Note-Taker
 saves personal notes
+
+
+
+install uuid, 
+Initialize a new Node.js project by running npm init -y in your project directory.
+
+Install Express.js by running npm install express
---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+const PORT = 3000
+
+const dbFilePath = path.join(__dirname, './db/db.json')
+
+// requires the uuid package
+const { v4: uuidv4 } = require('uuid')
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+
+
+// API ROUTES
+// sets up a route that handles GET requests to /api/notes
+//
+// get is used for retrieving the data
+//  /api/notes: Represents the endpoint path that the client will use to access this specific API route
+app.get('/api/notes', (req, res) => {
+    // reads the contents of the db.json file
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Internal server error' })
+        }
+
+        // parse the JSON data from db.json
+        const notes = JSON.parse(data)
+
+        // send the notes as JSON response
+        res.json(notes)
+    })
+})
+
+//
+// complete function! - 
+// generates a unique ID for the new note that has been submitted via a post /api/notes request
+let notes = []
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+    newNote.id = uuidv4() // here is where a unique ID is generated
+
+    notes.push(newNote)
+
+    console.log('Notes Array:', notes); // Log the notes array
+
+    res.json(newNote)
+})
+
+// HTML ROUTES
+// Define a route in Express that serves the notes.html file when a GET request is made
+// callback function  that is executed when a GET request is made to /notes
+app.get('/notes', (req, res) => {
+    // sends the notes.html file as the response when the /notes route is accessed
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+
+// wildcard route that will serve the index.html file for a path requested
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+
+// sets up server to listen on a specific port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+
+
